681e35d3e2e374a44fb5881c91e2b7ce83c21026
Sandeep kumar <skverma895746@gmail.com>
2026-01-15 17:44:36 +0530

    Update script.js
    Signed-off-by: Sandeep kumar <skverma895746@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-const Form = document.getElementById("contactForm");
-const errorMessage = document.getElementById("error-message");
 
+document.addEventListener("DOMContentLoaded",()=>
+{
+  // *********************
+const Form = document.getElementById("contactForm");
+if(Form){
 Form.addEventListener("submit", function (e) {
   e.preventDefault();
+  const errorMessage = document.getElementById("error-message");
   const agreeCheckbox = document.getElementById("agree");
   if (!agreeCheckbox.checked) {
     errorMessage.textContent = "Please agree to the Terms & Conditions ❗";
@@ -20,15 +24,17 @@ Form.addEventListener("submit", function (e) {
     if (response.ok) {
        errorMessage.textContent = "Message sent successfully ✅";
        errorMessage.style.color = "green";
-        clearMessage();
-      Form.reset();
+        clearMessage();   
     } else {
        errorMessage.textContent = "Something went wrong ❌";
        errorMessage.style.color = "red";
        clearMessage();
     }
   });
+   Form.reset();
 });
+}
+
 // clearmessage 
 function clearMessage(){
 setTimeout(() => {
@@ -36,7 +42,10 @@ setTimeout(() => {
 }, 2000);
 }
 // shareWeb site
-function shareWebsite(){
+const shareWebsite = document.querySelector("#shareWebsite");
+if(shareWebsite){
+  shareWebsite.addEventListener("click",()=>
+  {
   if(navigator.share){
     navigator.share({
       title:document.title,
@@ -46,4 +55,19 @@ function shareWebsite(){
   else{
     alert("share option  is not supported ");
   }
+  });
 }
+// mobile menu 
+const SmallMenu = document.querySelector("#navbar #menu-icon");
+const MenuItems = document.querySelector("#navbar .navbar-items");
+if(SmallMenu && MenuItems) {
+SmallMenu.addEventListener("click",function(){
+MenuItems.classList.toggle("active");
+MenuItems.classList.contains("active")? SmallMenu.innerHTML = `<i class="fa-solid fa-xmark"></i>`: SmallMenu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+});
+}
+//  ***********************
+});
+
+
+
